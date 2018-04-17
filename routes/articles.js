0462feecee6ea01d3510fd830d55c5cdd3cf3987
David Cao <David.Cao22@gmail.com>
2018-04-17 14:16:01 -0500

    _WIP_ Implemented Most API Endpoints For Single Article

--- routes/articles.js
+++ routes/articles.js
@@ -4,14 +4,18 @@ const monk = require("monk");
 const router = express.Router();
 const db = monk("localhost:27017/Redigg");
 
+// get all articles
 router.get("/", (req, res) => {
     const collection = db.get("articles");
-    collection.find({}, (err, articles) => {
-        if (err) throw err;
-        res.json(articles);
+    collection.find(
+        {},
+        (err, articles) => {
+            if (err) throw err;
+            res.json(articles);
     });
 });
 
+// add new article
 router.post("/", (req, res) => {
     const article = {
         title: req.body.title,
@@ -21,10 +25,86 @@ router.post("/", (req, res) => {
         comments: req.body.comments
     };
     const collection = db.get("articles");
-    collection.insert(article, (err, dbArticle) => {
-        if (err) throw err;
-        res.json(dbArticle)
+    collection.insert(
+        article,
+        (err, dbArticle) => {
+            if (err) throw err;
+            res.json(dbArticle)
     })
+});
+
+// get article by id
+router.get("/:articleid", (req, res) => {
+    const collection = db.get("articles");
+    collection.find(
+        {_id: req.params.articleid},
+        (err, article) => {
+            if (err) throw err;
+            res.json(article);
+    });
+});
+
+// add comment to article by article id
+router.post("/:articleid", (req, res) => {
+    const collection = db.get("articles");
+    // get article
+    collection.find(
+        {_id: req.params.articleid},
+        (err, article) => {
+            if (err) throw err;
+            // increment count of comments in article to get new id
+            const newCommentId = article[0].comments.length++;
+            // create payload and perform update
+            const comment = {
+                commentid: newCommentId,
+                body: req.body.body,
+                date: req.body.date,
+                user: req.body.user,
+                votes: 0
+            };
+            collection.update(
+                {_id: req.params.articleid},
+                {$push: {comments: comment}},
+                (err, article) => {
+                    if (err) throw err;
+                    res.json(article);
+            });
+    })
+});
+
+// delete article
+router.delete("/:articleid", (req, res) => {
+    const collection = db.get("articles");
+    collection.remove(
+        {_id: req.params.articleid},
+        (err, article) => {
+            if (err) throw err;
+            res.json(article);
+    });
+});
+
+//TODO TEST ME
+router.put("/:articleid", (req, res) => {
+    const collection = db.get("articles");
+    if(req.body.vote == true) {
+        //upvote
+        collection.update(
+            {_id: req.params.articleid},
+            {$inc: {votes: 1}},
+            (err, article) => {
+                if (err) throw err;
+                res.json(article);
+        });
+    } else {
+        //downvote
+        collection.update(
+            {_id: req.params.articleid},
+            {$inc: {votes: -1}},
+            (err, article) => {
+                if (err) throw err;
+                res.json(article);
+        });
+    }
 });
 
 module.exports = router;
